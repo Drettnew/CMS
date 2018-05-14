c1db238684500a57c618d10ca664d1e16f5aec13
drettnew <drettnew@gmail.com>
2018-05-14 18:54:17 +0200

    Add, Edit, Delete Names

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -30,22 +30,25 @@
         {
             this.components = new System.ComponentModel.Container();
             this.mainGridView = new System.Windows.Forms.DataGridView();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.employeesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.certificationsDatabaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.certificationsDatabaseDataSet = new CMS.CertificationsDatabaseDataSet();
             this.employeesTableAdapter = new CMS.CertificationsDatabaseDataSetTableAdapters.EmployeesTableAdapter();
             this.menuButton = new System.Windows.Forms.Button();
             this.jobMenu = new System.Windows.Forms.Panel();
-            this.certificationsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panelEmployees = new System.Windows.Forms.Panel();
+            this.buttonControl = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.certificationsTableAdapter = new CMS.CertificationsDatabaseDataSetTableAdapters.CertificationsTableAdapter();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.mainGridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.certificationsDatabaseDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.certificationsDatabaseDataSet)).BeginInit();
-            this.jobMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.certificationsBindingSource)).BeginInit();
+            this.panelEmployees.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainGridView
@@ -57,23 +60,15 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.mainGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.mainGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
             this.name});
             this.mainGridView.Location = new System.Drawing.Point(10, 45);
+            this.mainGridView.MultiSelect = false;
             this.mainGridView.Name = "mainGridView";
             this.mainGridView.ReadOnly = true;
+            this.mainGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.mainGridView.Size = new System.Drawing.Size(963, 500);
             this.mainGridView.TabIndex = 1;
-            // 
-            // name
-            // 
-            this.name.HeaderText = "Name";
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
-            // 
-            // employeesBindingSource
-            // 
-            this.employeesBindingSource.DataMember = "Employees";
-            this.employeesBindingSource.DataSource = this.certificationsDatabaseDataSetBindingSource;
             // 
             // certificationsDatabaseDataSetBindingSource
             // 
@@ -102,38 +97,107 @@
             // 
             // jobMenu
             // 
-            this.jobMenu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.jobMenu.BackColor = System.Drawing.SystemColors.ControlLight;
             this.jobMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.jobMenu.Controls.Add(this.checkedListBox1);
-            this.jobMenu.Location = new System.Drawing.Point(724, 45);
+            this.jobMenu.Location = new System.Drawing.Point(746, 512);
             this.jobMenu.Name = "jobMenu";
             this.jobMenu.Size = new System.Drawing.Size(250, 500);
             this.jobMenu.TabIndex = 3;
             // 
-            // certificationsBindingSource
+            // panelEmployees
             // 
-            this.certificationsBindingSource.DataMember = "Certifications";
-            this.certificationsBindingSource.DataSource = this.certificationsDatabaseDataSetBindingSource;
+            this.panelEmployees.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelEmployees.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEmployees.Controls.Add(this.buttonControl);
+            this.panelEmployees.Controls.Add(this.textBoxName);
+            this.panelEmployees.Controls.Add(this.label1);
+            this.panelEmployees.Location = new System.Drawing.Point(725, 45);
+            this.panelEmployees.Name = "panelEmployees";
+            this.panelEmployees.Size = new System.Drawing.Size(250, 500);
+            this.panelEmployees.TabIndex = 4;
+            // 
+            // buttonControl
+            // 
+            this.buttonControl.Location = new System.Drawing.Point(74, 88);
+            this.buttonControl.Name = "buttonControl";
+            this.buttonControl.Size = new System.Drawing.Size(75, 23);
+            this.buttonControl.TabIndex = 2;
+            this.buttonControl.Text = "button1";
+            this.buttonControl.UseVisualStyleBackColor = true;
+            this.buttonControl.Click += new System.EventHandler(this.buttonControl_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(3, 32);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(242, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
             // 
             // certificationsTableAdapter
             // 
             this.certificationsTableAdapter.ClearBeforeFill = true;
             // 
-            // checkedListBox1
+            // buttonNew
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(3, 3);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(110, 94);
-            this.checkedListBox1.TabIndex = 1;
+            this.buttonNew.Location = new System.Drawing.Point(13, 12);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(75, 23);
+            this.buttonNew.TabIndex = 4;
+            this.buttonNew.Text = "New";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(95, 13);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 5;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(177, 13);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Visible = false;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(987, 566);
+            this.Controls.Add(this.panelEmployees);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonNew);
             this.Controls.Add(this.jobMenu);
             this.Controls.Add(this.menuButton);
             this.Controls.Add(this.mainGridView);
@@ -141,11 +205,10 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.mainGridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.employeesBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.certificationsDatabaseDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.certificationsDatabaseDataSet)).EndInit();
-            this.jobMenu.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.certificationsBindingSource)).EndInit();
+            this.panelEmployees.ResumeLayout(false);
+            this.panelEmployees.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -154,14 +217,19 @@
         private System.Windows.Forms.DataGridView mainGridView;
         private System.Windows.Forms.BindingSource certificationsDatabaseDataSetBindingSource;
         private CertificationsDatabaseDataSet certificationsDatabaseDataSet;
-        private System.Windows.Forms.BindingSource employeesBindingSource;
         private CertificationsDatabaseDataSetTableAdapters.EmployeesTableAdapter employeesTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.Button menuButton;
         private System.Windows.Forms.Panel jobMenu;
-        private System.Windows.Forms.BindingSource certificationsBindingSource;
         private CertificationsDatabaseDataSetTableAdapters.CertificationsTableAdapter certificationsTableAdapter;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Panel panelEmployees;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonControl;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -16,8 +16,15 @@ namespace CMS
 {
     public partial class FormMain : Form
     {
-        bool menu_show = false;
-        int menu_size = 0;
+        static string conStr = ConfigurationManager.ConnectionStrings["CMS.Properties.Settings.CertificationsDatabaseConnectionString"].ConnectionString;
+        SqlConnection con = new SqlConnection(conStr);
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        bool menu_job_show = false;
+        int menu_job_size = 0;
+        bool menu_Employee_show = false;
+        int menu_Employee_size = 0;
         public FormMain()
         {
             InitializeComponent();
@@ -30,55 +37,64 @@ namespace CMS
 
         private void FormMain_Load(object sender, EventArgs e)
         {
+            cmd.Connection = con;
+            InitDataTable();
             fillTable();
+
+            menu_job_size = jobMenu.Width;
+            jobMenu.Width = 0;
+
+            menu_Employee_size = panelEmployees.Width;
+            panelEmployees.Width = 0;
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            if (!menu_show)
+            if (!menu_job_show)
             {
-                mainGridView.Width = mainGridView.Width - menu_size;
-                jobMenu.Width = menu_size;
-                menu_show =! menu_show;
+                mainGridView.Width = mainGridView.Width - menu_job_size;
+                jobMenu.Width = menu_job_size;
+                menu_job_show = !menu_job_show;
             }
             else
             {
-                mainGridView.Width = mainGridView.Width + menu_size;
+                mainGridView.Width = mainGridView.Width + menu_job_size;
                 jobMenu.Width = 0;
-                menu_show = !menu_show;
+                menu_job_show = !menu_job_show;
             }
-            
         }
 
-        private void fillTable()
+        private void InitDataTable()
         {
-            this.certificationsTableAdapter.Fill(this.certificationsDatabaseDataSet.Certifications);
-            menu_size = jobMenu.Width;
-            jobMenu.Width = 0;
-
-            string conStr = ConfigurationManager.ConnectionStrings["CMS.Properties.Settings.CertificationsDatabaseConnectionString"].ConnectionString;
-            SqlConnection con = new SqlConnection(conStr);
             con.Open();
+            cmd.CommandText = "select Name from Certifications";
+            dr = cmd.ExecuteReader();
 
-            string sqlQuery = "select Name from Certifications";
-            SqlCommand cmd = new SqlCommand(sqlQuery, con);
-            SqlDataReader dr = cmd.ExecuteReader();
             int i = 0;
             while (dr.Read())
             {
                 mainGridView.Columns.Add(dr["Name"].ToString(), dr["Name"].ToString());
             }
             dr.Close();
+            con.Close();
+        }
 
-            cmd.CommandText = "select Name from Employees";
+        private void fillTable()
+        {
+            mainGridView.Rows.Clear();
+            mainGridView.Refresh();
+            
+            con.Open();
+            cmd.CommandText = "select Id, Name from Employees";
             dr = cmd.ExecuteReader();
 
             Dictionary<string, int> employeeListIndex = new Dictionary<string, int>();
             while (dr.Read())
             {
                 int index = mainGridView.Rows.Add();
-                mainGridView.Rows[index].Cells["Name"].Value = dr[0].ToString();
-                employeeListIndex.Add(dr[0].ToString(), index);
+                mainGridView.Rows[index].Cells["Id"].Value = dr[0].ToString();
+                mainGridView.Rows[index].Cells["Name"].Value = dr[1].ToString();
+                employeeListIndex.Add(dr[1].ToString(), index);
             }
 
             dr.Close();
@@ -105,6 +121,94 @@ namespace CMS
             }
             dr.Close();
             con.Close();
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (mainGridView.SelectedRows.Count > 0 && !menu_Employee_show)
+            {
+                mainGridView.Width = mainGridView.Width - menu_Employee_size;
+                panelEmployees.Width = menu_Employee_size;
+                menu_Employee_show = !menu_Employee_show;
+
+                buttonControl.Text = "Confirm Edit";
+                textBoxName.Text = mainGridView.SelectedRows[0].Cells["Name"].Value.ToString();
+            }
+            else if(mainGridView.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("You have to select a row for too edit!");
+            }else if (menu_Employee_show)
+            {
+                mainGridView.Width = mainGridView.Width + menu_Employee_size;
+                panelEmployees.Width = 0;
+                menu_Employee_show = !menu_Employee_show;
+            }
+        }
+
+        private void buttonControl_Click(object sender, EventArgs e)
+        {
+            if (buttonControl.Text == "Confirm Edit")
+            {
+                cmd.CommandText = "update Employees set Name = \'" + textBoxName.Text + "\' where Id = " + mainGridView.SelectedRows[0].Cells["Id"].Value.ToString();
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                fillTable();
+
+                mainGridView.Width = mainGridView.Width + menu_Employee_size;
+                panelEmployees.Width = 0;
+                menu_Employee_show = !menu_Employee_show;
+            }
+            else if (buttonControl.Text == "Add New!")
+            {
+                cmd.CommandText = "insert into Employees (Name) values (\'" + textBoxName.Text + "\');";
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                fillTable();
+
+                mainGridView.Width = mainGridView.Width + menu_Employee_size;
+                panelEmployees.Width = 0;
+                menu_Employee_show = !menu_Employee_show;
+            }
+
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            if (mainGridView.SelectedRows.Count > 0 && !menu_Employee_show)
+            {
+                mainGridView.Width = mainGridView.Width - menu_Employee_size;
+                panelEmployees.Width = menu_Employee_size;
+                menu_Employee_show = !menu_Employee_show;
+
+                buttonControl.Text = "Add New!";
+            }
+            else if (mainGridView.SelectedRows.Count <= 0)
+            {
+                MessageBox.Show("You have to select a row for too edit!");
+            }
+            else if (menu_Employee_show)
+            {
+                mainGridView.Width = mainGridView.Width + menu_Employee_size;
+                panelEmployees.Width = 0;
+                menu_Employee_show = !menu_Employee_show;
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "delete from Employees where Id = " + mainGridView.SelectedRows[0].Cells["Id"].Value.ToString();
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            fillTable();
         }
     }
 }
