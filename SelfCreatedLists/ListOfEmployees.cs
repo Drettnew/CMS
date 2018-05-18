86eb89d68713a6ceba7812446562433fef1c0794
Shikyo <dennan_lol@hotmail.com>
2018-05-18 23:50:59 +0200

    New container
    New container

---
+++ SelfCreatedLists/ListOfEmployees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.SelfCreatedLists
+{
+    class ListOfEmployees
+    {
+        public List<String> names;
+        public List<List<String>> cert;
+
+        public ListOfEmployees()
+        {
+            names = new List<String>();
+            cert = new List<List<String>>();
+        }
+    }
+}
